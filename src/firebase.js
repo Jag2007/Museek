56b217f1d76b285fd72133274a9e0618f8065d2f
jagruthip2007 <jagruthipulumati@Jagruthis-MacBook-Pro.local>
2025-07-03 15:16:17 +0530

    changed again

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,17 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_ifMg6obK5u07HJVfjJy22DBDgGoIpmc",
-  authDomain: "museek-da659.firebaseapp.com",
-  projectId: "museek-da659",
-  storageBucket: "museek-da659.firebasestorage.app",
-  messagingSenderId: "530975522796",
-  appId: "1:530975522796:web:156569d145591f5bcccac9",
-  measurementId: "G-VK66L581M1",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider(); // ✅ use this
